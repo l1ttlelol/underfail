b14c824c604a7e7935628c0f3a0e2296095f28ce
Louis <louis@cowlibob.co.uk>
2018-04-06 19:42:16 +0100

    creating a bullet drop off
    by changing the x and y axis change I was able to create a drop off

--- game_template.py
+++ game_template.py
@@ -14,9 +14,11 @@ PI = 3.141592653
 health = 100
 rect_x = 40
 rect_y = 40
+rect_change_x = 10
+rect_change_y = 0
 
 #creating the window
-size = (700,500)
+size = (1400,1000)
 screen = pygame.display.set_mode(size) 
 pygame.display.set_caption("template")
 done = False
@@ -37,8 +39,10 @@ while not done:
 	#text = font.render("Player",True,White)
 	#screen.blit(text,[95,60])
 	pygame.draw.rect(screen, Red, [rect_x,rect_y,10,5])
-	rect_x += 5
-	rect_y += 4
+	rect_x += rect_change_x
+	rect_y += rect_change_y
+	rect_change_x += 1
+	rect_change_y += 0.5
 	pygame.display.flip()		                 
 	
 	#quitting the window part 2
