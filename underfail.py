31f1fa673d9da0b82f044c2c0828e533cec911f8
Louis <louis@cowlibob.co.uk>
2018-04-19 19:18:48 +0100

    defined a hitbox and the detection for a projectile

--- underfail.py
+++ underfail.py
@@ -14,6 +14,7 @@ player_y = (700)
 player = (player_x,player_y,player_health)
 projectile_x = random.randrange(400,1520)
 projectile_y = random.randrange(400,980)
+hit_box = pygame.Rect(player_x,player_y,70,70)
 
 size = (1920,1080)
 screen = pygame.display.set_mode(size) 
@@ -24,6 +25,7 @@ y_acceleration = 0
 x_acceleration = 0
 			
 while not done:
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			screen.fill(Black)
@@ -32,7 +34,7 @@ while not done:
 
 	#if event.type == pygame.KEYDOWN:
 	keys=pygame.key.get_pressed()
-	if player_x < 1445:
+	if hit_box.x < 1445:
 		if keys[pygame.K_RIGHT]:
 			if x_acceleration < 10:
 				x_acceleration += 1
@@ -40,7 +42,7 @@ while not done:
 		if x_acceleration > 0:
 			x_acceleration = 0
 	
-	if player_x > 405:
+	if hit_box.x > 405:
 		if keys[pygame.K_LEFT]:
 			if x_acceleration > -10:
 				x_acceleration -=1
@@ -48,7 +50,7 @@ while not done:
 		if x_acceleration < 0:
 			x_acceleration = 0
 
-	if player_y < 900:
+	if hit_box.y < 900:
 		if keys[pygame.K_DOWN]:
 			if y_acceleration < 10:
 				y_acceleration += 1
@@ -56,7 +58,7 @@ while not done:
 		if y_acceleration > 0:
 			y_acceleration = 0
 
-	if player_y > 410:
+	if hit_box.y > 410:
 		if keys[pygame.K_UP]:
 			if y_acceleration > -10:
 				y_acceleration -=1
@@ -68,12 +70,12 @@ while not done:
 		y_acceleration = 0
 
 	
-	player_x += x_acceleration
-	player_y += y_acceleration
+	hit_box.x += x_acceleration
+	hit_box.y += y_acceleration
 
 	screen.fill(Black)
 	pygame.draw.rect(screen,White,[400,400,1120,580],2)
-	pygame.draw.rect(screen,White,[player_x,player_y,70,70])
+	pygame.draw.rect(screen,White,hit_box)
  	#pygame.draw.line(screen,White,[projectile_x,projectile_y],[projectile_x + 2,projectile_y + 2],2)
 	if player_health < 1:
 		screen.fill(Black)
