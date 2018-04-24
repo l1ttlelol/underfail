d8a1dec34132b0477cea1109045f0a302def581d
Louis <louis@cowlibob.co.uk>
2018-04-24 19:59:14 +0100

    i have made a duplicate of the file that  will act as the main game

---
+++ unerfail_combat_simulater.py
@@ -0,0 +1,133 @@
+import pygame
+import random
+import time
+pygame.init()
+
+start_time = time.time()
+total_time = -1
+
+
+Black = (0,0,0)
+White = (255,255,255)
+ScreenWidth = 1920
+ScreenHeight = 1080
+
+
+boundary_x = 400
+boundary_y = 400
+boundary_length = 1120
+boundary_height = 580
+boundary_right = boundary_x + boundary_length
+
+player_health = (100)
+player_x = (700)
+player_y = (700)
+player = (player_x,player_y,player_health)
+player_health_deduction = 60
+
+def easy_difficulty():
+	player_health_deduction = 3
+	random.randint(0,9) == 0
+def hard_difficulty():
+	player_health_deduction = 6
+	random.randint(0,15) == 0
+def expert_difficulty():
+	player_health_deduction = 10
+	random.randint(0,20) == 0
+projectiles = []	
+hit_box = pygame.Rect(player_x,player_y,70,70)
+
+size = (ScreenWidth,ScreenHeight)
+screen = pygame.display.set_mode(size) 
+pygame.display.set_caption("underfail demo")
+done = False
+clock = pygame.time.Clock()
+y_acceleration = 0
+x_acceleration = 0
+font = pygame.font.SysFont('Calibri', 25, True, False)
+gameover_font = pygame.font.SysFont('Calibri', 50, True, False)
+			
+while not done:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			screen.fill(Black)
+			done = True
+
+
+	#if event.type == pygame.KEYDOWN:
+	keys=pygame.key.get_pressed()
+	if hit_box.x < 1445:
+		if keys[pygame.K_RIGHT]:
+			if x_acceleration < 10:
+				x_acceleration += 1
+	else:
+		if x_acceleration > 0:
+			x_acceleration = 0
+	
+	if hit_box.x > 405:
+		if keys[pygame.K_LEFT]:
+			if x_acceleration > -10:
+				x_acceleration -=1
+	else:
+		if x_acceleration < 0:
+			x_acceleration = 0
+
+	if hit_box.y < 900:
+		if keys[pygame.K_DOWN]:
+			if y_acceleration < 10:
+				y_acceleration += 1
+	else:
+		if y_acceleration > 0:
+			y_acceleration = 0
+
+	if hit_box.y > 410:
+		if keys[pygame.K_UP]:
+			if y_acceleration > -10:
+				y_acceleration -=1
+	else:
+		if y_acceleration < 0:
+			y_acceleration = 0
+	if event.type == pygame.KEYUP:
+		x_acceleration = 0
+		y_acceleration = 0
+
+	
+	hit_box.x += x_acceleration
+	hit_box.y += y_acceleration
+
+	if random.randint(0,9) == 0 and len(projectiles) < 15:
+		projectiles.append({'x': random.randrange(400,450), 'y': random.randrange(400,980)})
+
+	screen.fill(Black)
+	text = font.render("HEALTH",True,White)
+	pygame.draw.rect(screen,White,[350,250,player_health,20])
+	screen.blit(text,[250,250])
+
+	Deathtext = font.render("Game Over",True,White)
+	#screen.blit(Deathtext,[1250,250])
+
+	pygame.draw.rect(screen,White,[400,400, boundary_length, boundary_height],2)
+	pygame.draw.rect(screen,White,hit_box)
+	
+
+	# Loop through the projectiles and do stuff
+	for projectile in projectiles:
+		pygame.draw.line( screen,White,
+			[projectile['x'], projectile['y']], [projectile['x'] - 30, projectile['y'] ])
+		if projectile['x'] >= boundary_right:
+			projectiles.remove(projectile)
+		else:
+			projectile['x'] = projectile['x'] + 10
+		if hit_box.collidepoint(projectile['x'], projectile['y']):
+			player_health -= player_health_deduction
+
+	if player_health < 1:
+		screen.fill(Black)
+		screen.blit(Deathtext,[800,520])
+
+
+	pygame.display.flip()
+
+	clock.tick(60)
+pygame.quit()
